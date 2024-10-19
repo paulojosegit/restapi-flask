bae2b43663c146fcd74b58def8e288c5408cf351
paulojosegit <paulojosegm@gmail.com>
2024-10-19 20:14:32 -0300

    formating with flake8

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message': 'users'}
 
+
 class User(Resource):
     def get(self):
         return {'message': 'cpf'}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user/')
